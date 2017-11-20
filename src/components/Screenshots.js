c84bd1ad27369649f8bdba929beab17ef882362e
Grøttum <grotar@egmont.com>
2017-11-20 17:02:43 +0100

    working on css animation with support for callback on end of scroll

--- src/components/Screenshots.js
+++ src/components/Screenshots.js
@@ -4,29 +4,37 @@ import '../css/Screenshots.css';
 export default class Screenshots extends Component {
   constructor(props) {
     super(props);
-    this.state = { img:
-      {
-        src:'NaN'
-      }
+    this.state = {
+      scroll:false,
+      sections : ['', 'helse', 'motor', 'bolig', 'mote', 'mat', 'teknologi'],
+      idx: 0,
     };
+    this.scrollDone = this.scrollDone.bind(this);
   }
 
   componentDidMount() {
-    fetch('/imgages/klikk_.png')
-      .then(res => res.blob())
-      .then(imgBlob => {
-        let objectURL = URL.createObjectURL(imgBlob);
-        this.setState({img: {src: objectURL}})}
-      );
+    const el = this.refs.button;
+    el.addEventListener('animationend', this.scrollDone);
+  }
+  componentWillUnmount () {
+  	const el = this.refs.button
+		el.removeEventListener('animationend', this.scrollDone)
+	}
+
+  scrollDone () {
+    this.setState({
+      scroll:false,
+      idx: this.state.idx + 1
+    });
   }
 
 
-
-
   render() {
+    const scroll = this.state.scroll;
     return (
       <div className="screenshot">
-        <img className="shimg" src={new Image().src ="/images/klikk_.png"}/>
+        <button ref='button' onClick={() => this.setState({scroll:true})}>Scrollway to Hell</button>
+        <img className={scroll? 'scroll':''} src={new Image().src ='/images/klikk_' + this.state.sections[this.state.idx] + '.png'}/>
       </div>
     );
   }
