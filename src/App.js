cf5bc84581de90bc2ad7dd254815ba9e1e883e17
Grøttum <grotar@egmont.com>
2017-11-24 13:00:12 +0100

    signing off Friday (new component Stats)

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import './App.css';
 import Screenshots from './components/Screenshots.js';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      sections : ['', 'helse', 'motor', 'bolig', 'mote', 'mat', 'teknologi']
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        
         <Screenshots/>
+        <Stats/>
       </div>
     );
   }
